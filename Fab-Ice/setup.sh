86e94081ef529ac2d8b5327fcf63f9f508de85c3
kioryu <1218moonlight@gmail.com>
2019-05-17 11:31:19 +0900

    [ update ] setup.sh

--- Fab-Ice/setup.sh
+++ Fab-Ice/setup.sh
@@ -16,8 +16,8 @@ function logHelp() {
 
 function remove() {
     logInfo "remove()"
-    rm -rf channel-artifacts
-    rm -rf crypto-config
+    sudo rm -rf channel-artifacts
+    sudo rm -rf crypto-config
     echo
 }
 
@@ -47,14 +47,39 @@ if [[ ${MODE} == "build" ]]; then
     newGenesis
     newChannel
 elif [[ ${MODE} == "run" ]]; then
+    ORDERER_CA="/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem"
     sudo docker-compose -f docker-compose.yml up -d && \
+
     sudo docker exec \
-    -e ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
-    -it \
-    cli bash
+    cli \
+    peer channel create \
+    -o orderer.example.com:7050 \
+    -c channelall \
+    -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/channelall.tx \
+    --cafile ${ORDERER_CA} && \
+
+    sudo docker exec \
+    cli \
+    peer channel join \
+    -b channelall.block \
+    --cafile ${ORDERER_CA} && \
+
+    sudo docker exec \
+    cli \
+    peer channel update \
+    -o orderer.example.com:7050 \
+    -c channelall \
+    -f /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/Org1MSPanchors_channelall.tx \
+    --cafile ${ORDERER_CA} && \
+
+     sudo docker exec \
+     -e ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
+     -it \
+     cli bash
 elif [[ ${MODE} == "rm" ]]; then
     sudo docker-compose -f docker-compose.yml down && \
-    sudo docker rm $(docker ps -aq)
+    sudo docker rm $(sudo docker ps -aq)
+    sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
     remove
 else
     logHelp
