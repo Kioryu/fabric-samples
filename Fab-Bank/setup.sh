faa312a00061e715d3b10d546f2729795b7cce7d
kioryu <1218moonlight@gmail.com>
2019-05-27 11:08:22 +0900

    [ADD] Fab-Bank : NEW CHANNEL(ALL, VIP, SECRET)

--- Fab-Bank/setup.sh
+++ Fab-Bank/setup.sh
@@ -2,6 +2,15 @@
 
 MODE=$1
 
+CHANNEL_ALL_NAME=channelall
+CHANNEL_ALL_PROFILE=ChannelAll
+
+CHANNEL_VIP_NAME=channelvip
+CHANNEL_VIP_PROFILE=ChannelVIP
+
+CHANNEL_SECRET_NAME=channelsecret
+CHANNEL_SECRET_PROFILE=ChannelSecret
+
 function infoLog(){
     echo "[INFO] : $1"
 }
@@ -19,6 +28,21 @@ function main() {
     ../bin/cryptogen generate --config=./crypto-config.yaml
 
     ../bin/configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+
+    ../bin/configtxgen -profile ${CHANNEL_ALL_PROFILE} -outputCreateChannelTx ./channel-artifacts/${CHANNEL_ALL_NAME}.tx -channelID ${CHANNEL_ALL_NAME}
+
+    ../bin/configtxgen -profile ${CHANNEL_ALL_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors_${CHANNEL_ALL_NAME}.tx -channelID ${CHANNEL_ALL_NAME} -asOrg Org1MSP
+    ../bin/configtxgen -profile ${CHANNEL_ALL_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors_${CHANNEL_ALL_NAME}.tx -channelID ${CHANNEL_ALL_NAME} -asOrg Org2MSP
+    ../bin/configtxgen -profile ${CHANNEL_ALL_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors_${CHANNEL_ALL_NAME}.tx -channelID ${CHANNEL_ALL_NAME} -asOrg Org3MSP
+
+    ../bin/configtxgen -profile ${CHANNEL_VIP_PROFILE} -outputCreateChannelTx ./channel-artifacts/${CHANNEL_VIP_NAME}.tx -channelID ${CHANNEL_VIP_NAME}
+
+    ../bin/configtxgen -profile ${CHANNEL_VIP_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors_${CHANNEL_VIP_NAME}.tx -channelID ${CHANNEL_VIP_NAME} -asOrg Org1MSP
+    ../bin/configtxgen -profile ${CHANNEL_VIP_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors_${CHANNEL_VIP_NAME}.tx -channelID ${CHANNEL_VIP_NAME} -asOrg Org2MSP
+
+    ../bin/configtxgen -profile ${CHANNEL_SECRET_PROFILE} -outputCreateChannelTx ./channel-artifacts/${CHANNEL_SECRET_NAME}.tx -channelID ${CHANNEL_SECRET_NAME}
+
+    ../bin/configtxgen -profile ${CHANNEL_SECRET_PROFILE} -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors_${CHANNEL_SECRET_NAME}.tx -channelID ${CHANNEL_SECRET_NAME} -asOrg Org1MSP
 }
 
 if [[ ${MODE} == "rm" ]]; then
