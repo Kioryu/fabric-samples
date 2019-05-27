eadbc389808253d1393af19805b60b4e3e8aa955
kioryu <1218moonlight@gmail.com>
2019-05-27 17:26:14 +0900

    sendMoney 기능 추가. sendMoney 機能追加。

--- chaincode/Fab-Bank/fabbank.go
+++ chaincode/Fab-Bank/fabbank.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
 	"encoding/json"
+	"strconv"
 )
 
 type SmartContract struct {
@@ -48,11 +49,14 @@ func (s *SmartContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
 		return s.open(stub, args)
 	} else if function == "getBankBook" {
 		return s.getBankBook(stub, args)
+	} else if function == "sendMoney" {
+		return s.sendMoney(stub, args)
 	}
 
 	return shim.Error("Invalid Smart Contract function name.")
 }
 
+// from(key), pwd, identifier, balance
 func (s *SmartContract) open(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments. Expecting 4")
@@ -84,6 +88,7 @@ func (s *SmartContract) open(stub shim.ChaincodeStubInterface, args []string) pe
 	return shim.Success(result)
 }
 
+// from(key), pwd
 func (s *SmartContract) getBankBook(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 2")
@@ -105,6 +110,91 @@ func (s *SmartContract) getBankBook(stub shim.ChaincodeStubInterface, args []str
 
 	return shim.Success(result)
 
+}
+
+// from, pwd, to, value
+func (s *SmartContract) sendMoney(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 4")
+	}
+	from := args[0]
+	pwd := args[1]
+	to := args[2]
+	value := args[3]
+
+	fromResult, err := stub.GetState(from)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	if fromResult == nil {
+		return shim.Error(fmt.Sprintf("Empty Key %s", from))
+	}
+
+	err = checkPWD(fromResult, pwd)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	toResult, err := stub.GetState(to)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	if toResult == nil {
+		return shim.Error(fmt.Sprintf("Empty To %s", to))
+	}
+
+	fromBook := bankbook{}
+	toBook := bankbook{}
+
+	json.Unmarshal(fromResult, &fromBook)
+	json.Unmarshal(toResult, &toBook)
+
+	fromBalance, err := strconv.ParseInt(fromBook.Balance, 10, 64)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	toBalance, err := strconv.ParseInt(toBook.Balance, 10, 64)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	sendValue, err := strconv.ParseInt(value, 10, 64)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	resultBalance := fromBalance - sendValue
+	if resultBalance < 0 {
+		return shim.Error(fmt.Sprintf("%s", "no money."))
+	}
+
+	fromBook.Balance = fmt.Sprintf("%d", resultBalance)
+	toBook.Balance = fmt.Sprintf("%d", toBalance+sendValue)
+
+	fromBookByte, err := json.Marshal(fromBook)
+	if err != nil {
+		shim.Error(err.Error())
+	}
+
+	toBookByte, err := json.Marshal(toBook)
+	if err != nil {
+		shim.Error(err.Error())
+	}
+
+	err = stub.PutState(from, fromBookByte)
+	if err != nil {
+		shim.Error(err.Error())
+	}
+
+	err = stub.PutState(to, toBookByte)
+	if err != nil {
+		shim.Error(err.Error())
+	}
+
+	return shim.Success([]byte("Money has been transferred."))
 }
 
 func main() {
