2a92f639184c945ba32d15a99c1b0b01b244921f
kioryu <1218moonlight@gmail.com>
2019-05-27 14:09:35 +0900

    [INIT] fabbank Chaincode

---
+++ chaincode/Fab-Bank/fabbank.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/protos/peer"
+)
+
+type SmartContract struct {
+}
+
+type bankbook struct {
+	Identifier string `json:"identifier"`
+	Pwd        string `json:"pwd"`
+	Balance    uint64 `json:"balance"`
+	Owner      string `json:"owner"`
+}
+
+func (s *SmartContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
+	return shim.Success(nil)
+}
+
+func (s *SmartContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+
+	return shim.Error("Invalid Smart Contract function name.")
+}
+
+func main() {
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+}
